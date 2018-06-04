8728dd0b0953fd8dd77d5888602a04ee3f016eb5
Obo Agboghidi <oagboghi2@gmail.com>
2018-06-04 15:46:58 -0700

    updated error messages

--- bears/bearController.js
+++ bears/bearController.js
@@ -9,16 +9,19 @@ router
       res.status(200).json(response);
     })
     .catch(err => {
-      res.status(500).json({ error: err})
+      res.status(500).json({ errorMessage: "The bear information could not be retrieved." })
     })
   })
   .post((req, res) => {
+    if(!species || !latinName){
+      return res.status(404).json({ errorMessage: "Please provide both species and latinName for the bear." });
+    }
     const { species, latinName } = req.body;
     const newBear = new Bear ({ species, latinName });
     newBear
       .save() //this will 'insert' a document into the Bear collection
       .then(savedBear => res.status(201).json({ status: 'please implement POST functionality' }))
-      .catch(err => res.status(422).json({ error: err}))
+      .catch(err => res.status(422).json({ errorMessage: "There was an error while saving the bear to the database"}))
   });
 
 router
@@ -26,22 +29,37 @@ router
   .get((req, res) => {
     const { id } = req.params;
     Bear.findById(id)//find a specifc resource by using their ID
-    .then(foundBear => res.status(200).json(foundBear))
-      .catch(err => res.status(500).json("error", {error: err}))
+    .then(foundBear => {
+      if (foundBear === 0) {
+        return res.status(404).json({ errorMessage: "The bear with the specified ID does not exist." });
+      }
+      res.status(200).json(foundBear)
+    })
+      .catch(err => res.status(500).json({ errorMessage: "The bear information could not be retrieved."}))
   })
   .delete((req, res) => {
     const { id } = req.params;
     Bear.findByIdAndRemove(id)
-      .then(deletedBear => res.status(200).json({ deletedBear }))
-      .catch(err => res.status(500).json("error", {error: err}))
+      .then(deletedBear => {
+        if (foundBear === 0) {
+          return res.status(404).json({ errorMessage: "The bear with the specified ID does not exist." });
+        }
+        res.status(200).json({ deletedBear })
+      })
+      .catch(err => res.status(500).json({ errorMessage: "The bear could not be removed"}))
   })
   .put((req, res) => {
     const { id } = req.params;
     const { species, latinName } = req.body;
     //const UpdatedBear = new Bear({ species, latinName });
     Bear.findByIdAndUpdate(id, req.body)
-      .then(UpdatedBear => res.status(200).json( UpdatedBear ))
-      .catch(err => res.status(500).json("error", { error: err }))
+    if (foundBear === 0) {
+      return res.status(404).json({ errorMessage: "The bear with the specified ID could not be updated." });
+    }
+      .then(UpdatedBear => {
+        res.status(200).json( UpdatedBear )
+      })
+        .catch(err => res.status(500).json({ errorMessage: "The bear could not be removed"}))
   });
 
 module.exports = router;
